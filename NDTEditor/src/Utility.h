965cfd873eb5b0afb24fc5d509cbcd9e616c30f1
jackson <63016612+fairybow@users.noreply.github.com>
2025-05-11 22:54:55 -0400

    Adding a visual cue and generated colors

--- NDTEditor/src/Utility.h
+++ NDTEditor/src/Utility.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <algorithm>
+#include <cmath>
+#include <numbers>
 
+#include <QColor>
 #include <QList>
 #include <QMargins>
 #include <QSet>
@@ -41,5 +44,40 @@ namespace Utility
     LayoutT* zeroPaddedLayout(QWidget* parent = nullptr, Qt::Alignment alignment = {})
     {
         return newLayout<LayoutT>({}, 0, parent, alignment);
+    }
+
+    inline QList<QColor> phiColors(int count, const QColor& startColor = Qt::red)
+    {
+        QList<QColor> result{};
+        if (count < 1) return result;
+
+        double h = startColor.hsvHueF();
+
+        // Different saturation and value levels for more variety
+        QList<double> saturations = { 0.9, 0.7, 0.8 };
+        QList<double> values = { 0.9, 0.8, 0.95 };
+
+        constexpr double conjugate = 1.0 / std::numbers::phi;
+
+        for (auto i = 0; i < count; ++i)
+        {
+            // Select saturation and value based on position
+            double s = saturations[i % saturations.size()];
+            double v = values[i % values.size()];
+
+            // Add color
+            result << QColor::fromHsvF(h, s, v);
+
+            // Advance hue by golden ratio conjugate
+            h += conjugate;
+
+            //if (h >= 1.0)
+                //h -= 1.0;
+
+            // Keep h in the range [0, 1) using fmod
+            h = fmod(h, 1.0);
+        }
+
+        return result;
     }
 }
